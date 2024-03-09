2d95a8e7a3f12e522d0cf3dcf56f3ad0f6ac5e01
midoon <harunkoding@gmail.com>
2024-03-09 15:36:16 +0700

    Migrations(transactions)

---
+++ migrations/20240309044322_create_table_transactions.up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `transactions` (
+    `id` VARCHAR(255) NOT NULL,
+    `sof_number` VARCHAR(20) NOT NULL,
+    `dof_number` VARCHAR(20) NOT NULL,
+    `amount` INT NOT NULL,
+    `transaction_type` VARCHAR(1) NOT NULL ,
+    `account_id` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE `transactions` ADD CONSTRAINT `transactions_account_id_fkey` FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
